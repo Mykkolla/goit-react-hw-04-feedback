cb196198a9e0a80ea65a0a3d4c76a584fbaeac83
Mykkolla <tarabukin.n.10@gmail.com>
2023-05-23 19:28:51 +0300

    a

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,95 +1,55 @@
-import React, { Component } from 'react';
+// import React from 'react';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section/Section';
 import Notification from './NotificationMessage/NotificationMessage';
+import { useState } from 'react';
 
-export class FeedBackApp extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export default function FeedBackApp() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  // handleGoodFeedback = () => {
-  //   this.setState(prevState => ({
-  //     good: prevState.good + 1,
-  //   }));
-  // };
-  // handleNeutralFeedback = () => {
-  //   this.setState(prevState => ({
-  //     neutral: prevState.neutral + 1,
-  //   }));
-  // };
-  // handleBadFeedback = () => {
-  //   this.setState(prevState => ({
-  //     bad: prevState.bad + 1,
-  //   }));
-  // };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  function countTotalFeedback() {
     return good + neutral + bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
-    return total === 0 ? 0 : Math.round((good / total) * 100);
-  };
-
-  // handleLeaveFeedback = option => {
-  //   switch (option) {
-  //     case 'good':
-  //       this.setState(prevState => ({
-  //         good: prevState.good + 1,
-  //       }));
-  //       break;
-  //     case 'neutral':
-  //       this.setState(prevState => ({
-  //         neutral: prevState.neutral + 1,
-  //       }));
-  //       break;
-  //     case 'bad':
-  //       this.setState(prevState => ({
-  //         bad: prevState.bad + 1,
-  //       }));
-  //       break;
-  //     default:
-  //       return;
-  //   }
-  // };
-  handleLeaveFeedback = option => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-    const options = Object.keys(this.state);
-    console.log(options);
-
-    return (
-      <Section title="Please live feedback">
-        <FeedbackOptions
-          options={options}
-          onLeaveFeedback={this.handleLeaveFeedback}
-        />
-        {total > 0 ? (
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={positivePercentage}
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </Section>
-    );
   }
+
+  function countPositiveFeedbackPercentage() {
+    const total = countTotalFeedback();
+    return total === 0 ? 0 : Math.round((good / total) * 100);
+  }
+
+  const handleLeaveFeedback = option => {
+    if (option === 'good') {
+      setGood(prevGood => prevGood + 1);
+    } else if (option === 'neutral') {
+      setNeutral(prevNeutral => prevNeutral + 1);
+    } else if (option === 'bad') {
+      setBad(prevBad => prevBad + 1);
+    }
+  };
+
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+  const options = Object.keys({ good, neutral, bad });
+
+  return (
+    <Section title="Please live feedback">
+      <FeedbackOptions
+        options={options}
+        onLeaveFeedback={handleLeaveFeedback}
+      />
+      {total > 0 ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positivePercentage={positivePercentage}
+        />
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </Section>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FeedBackApp } from 'components/App';
+import FeedBackApp from 'components/App';
 import { GlobalStyle } from 'components/Style/Global';
 import 'modern-normalize';
 import './index.css';
